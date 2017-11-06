63c5382ce44a1171dad3c01bd8843c1194f38fcb
Peter Keklak <peter.keklak@i.cz>
2017-11-06 09:59:42 +0100

    Commit

--- src/main/java/cz/keklak/SpringBootApp/ReservationRepository.java
+++ src/main/java/cz/keklak/SpringBootApp/ReservationRepository.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cz.keklak.SpringBootApp;
 
 import java.util.Collection;
@@ -11,10 +6,6 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
-/**
- *
- * @author pkeklak
- */
 @RepositoryRestResource
 interface ReservationRepository extends JpaRepository<Reservation, Long> {
     @RestResource(path = "by-name")
